33a8e25e41aa2ef9c6a2d071b382ae2c2e04e13a
Jaime Frías <jaimefrias_76@hotmail.com>
2016-10-18 10:35:28 +0200

    Add files via upload

---
+++ lib/prize.rb
@@ -0,0 +1,13 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Prize
+  @@levels
+  @@treasures
+  
+  def initialize(treasures, levels)
+    @levels = levels;
+    @treasures = treasures;
+  end
+end
